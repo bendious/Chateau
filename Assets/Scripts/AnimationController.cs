6ab05e36c2567ea89e9ee106292b4a90b81bc823
bendious <benjamin.laws@comcast.net>
2021-12-08 10:42:17 -0600

    Formatting cleanups
    - spaces --> tabs
    - replaced 'var' w/ explicit types
    - added curly braces to single-line branches

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -9,67 +9,67 @@ namespace Platformer.Mechanics
 	/// AnimationController integrates physics and animation. It is generally used for simple enemy animation.
 	/// </summary>
 	[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
-    public class AnimationController : KinematicObject
-    {
-        /// <summary>
-        /// Max horizontal speed.
-        /// </summary>
-        public float maxSpeed = 7;
-        /// <summary>
-        /// Max jump velocity
-        /// </summary>
-        public float jumpTakeOffSpeed = 7;
+	public class AnimationController : KinematicObject
+	{
+		/// <summary>
+		/// Max horizontal speed.
+		/// </summary>
+		public float maxSpeed = 7;
+		/// <summary>
+		/// Max jump velocity
+		/// </summary>
+		public float jumpTakeOffSpeed = 7;
 
-        /// <summary>
-        /// Used to indicated desired direction of travel.
-        /// </summary>
-        public Vector2 move;
+		/// <summary>
+		/// Used to indicated desired direction of travel.
+		/// </summary>
+		public Vector2 move;
 
-        /// <summary>
-        /// Set to true to initiate a jump.
-        /// </summary>
-        public bool jump;
+		/// <summary>
+		/// Set to true to initiate a jump.
+		/// </summary>
+		public bool jump;
 
-        /// <summary>
-        /// Set to true to set the current jump velocity to zero.
-        /// </summary>
-        public bool stopJump;
+		/// <summary>
+		/// Set to true to set the current jump velocity to zero.
+		/// </summary>
+		public bool stopJump;
 
-        SpriteRenderer spriteRenderer;
-        Animator animator;
-        readonly PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+		SpriteRenderer spriteRenderer;
+		Animator animator;
+		readonly PlatformerModel model = Simulation.GetModel<PlatformerModel>();
 
-        protected virtual void Awake()
-        {
-            spriteRenderer = GetComponent<SpriteRenderer>();
-            animator = GetComponent<Animator>();
-        }
+		protected virtual void Awake()
+		{
+			spriteRenderer = GetComponent<SpriteRenderer>();
+			animator = GetComponent<Animator>();
+		}
 
-        protected override void ComputeVelocity()
-        {
-            if (jump && IsGrounded)
-            {
-                velocity.y = jumpTakeOffSpeed * model.jumpModifier;
-                jump = false;
-            }
-            else if (stopJump)
-            {
-                stopJump = false;
-                if (velocity.y > 0)
-                {
-                    velocity.y *= model.jumpDeceleration;
-                }
-            }
+		protected override void ComputeVelocity()
+		{
+			if (jump && IsGrounded)
+			{
+				velocity.y = jumpTakeOffSpeed * model.jumpModifier;
+				jump = false;
+			}
+			else if (stopJump)
+			{
+				stopJump = false;
+				if (velocity.y > 0)
+				{
+					velocity.y *= model.jumpDeceleration;
+				}
+			}
 
-            if (move.x > 0.01f)
-                spriteRenderer.flipX = false;
-            else if (move.x < -0.01f)
-                spriteRenderer.flipX = true;
+			if (move.x > 0.01f)
+				spriteRenderer.flipX = false;
+			else if (move.x < -0.01f)
+				spriteRenderer.flipX = true;
 
-            animator.SetBool("grounded", IsGrounded);
-            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+			animator.SetBool("grounded", IsGrounded);
+			animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
-            targetVelocity = move * maxSpeed;
-        }
-    }
+			targetVelocity = move * maxSpeed;
+		}
+	}
 }
