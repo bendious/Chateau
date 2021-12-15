7f0c2c87c2c4a94af6d5a9edec352e545d076ec7
bendious <benjamin.laws@comcast.net>
2021-12-15 10:05:48 -0600

    2D room spawning
    - updated room prefab to have "doors" at the top-/bottom-left
    	- also made all doors flush w/ their walls for ease of movement
    - RoomController.m_door{B/T}, m_{bottom/top}Open, and logic to randomly replace bottom/top doors w/ new rooms
    	- RoomController.m_spawnDepthMax to keep the number of spawned rooms to a managable level
    - RoomController.MaybeReplaceDoor() physics check to prevent spawning multiple rooms in the same space
    	- bounds/checkSize params
    		- CalculateBounds() private helper
    
    Todo: easier climbing & navigation, enemy climbing & pathfinding

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -12,26 +12,33 @@ public class RoomController : MonoBehaviour
 
 	public GameObject m_doorL;
 	public GameObject m_doorR;
+	public GameObject m_doorB;
+	public GameObject m_doorT;
 
 	public float m_roomSpawnPct = 0.5f;
+	public int m_spawnDepthMax = 5;
 
 
 	private bool m_leftOpen = false;
 	private bool m_rightOpen = false;
+	private bool m_bottomOpen = false;
+	private bool m_topOpen = false;
 
 
 	private void Start()
 	{
-		// TODO: check whether there is room to spawn rooms
-		bool fromLeft = m_leftOpen;
-		bool fromRight = m_rightOpen;
-		Assert.IsTrue(!fromLeft || !fromRight);
-		m_leftOpen = fromLeft || UnityEngine.Random.value > m_roomSpawnPct;
-		m_rightOpen = fromRight || UnityEngine.Random.value > m_roomSpawnPct;
+		// calculate size info
+		Bounds bounds = CalculateBounds();
+		Vector3 offsetMagH = new Vector3(bounds.size.x, 0.0f, 0.0f);
+		Vector3 offsetMagV = new Vector3(0.0f, bounds.size.y, 0.0f);
+		Vector3 checkSize = bounds.size - new Vector3(0.1f, 0.1f, 0.0f); // NOTE the small reduction to avoid always collecting ourself
 
-		Vector3 offsetMag = new Vector3(12.0f, 0.0f, 0.0f); // TODO: calculate from room size
-		MaybeReplaceDoor(m_leftOpen, m_doorL, !fromLeft, -offsetMag, roomController => roomController.m_rightOpen = true);
-		MaybeReplaceDoor(m_rightOpen, m_doorR, !fromRight, offsetMag, roomController => roomController.m_leftOpen = true);
+		// replace doors / spawn rooms
+		// TODO: randomize order to avoid directional bias?
+		MaybeReplaceDoor(ref m_leftOpen, bounds, -offsetMagH, checkSize, m_doorL, roomController => roomController.m_rightOpen = true);
+		MaybeReplaceDoor(ref m_rightOpen, bounds, offsetMagH, checkSize, m_doorR, roomController => roomController.m_leftOpen = true);
+		MaybeReplaceDoor(ref m_bottomOpen, bounds, -offsetMagV, checkSize, m_doorB, roomController => roomController.m_topOpen = true);
+		MaybeReplaceDoor(ref m_topOpen, bounds, offsetMagV, checkSize, m_doorT, roomController => roomController.m_bottomOpen = true);
 
 		// spawn items
 		// TODO: more deliberate spawning
@@ -59,9 +66,30 @@ public class RoomController : MonoBehaviour
 	}
 
 
-	private void MaybeReplaceDoor(bool remove, GameObject door, bool replace, Vector3 replaceOffset, Action<RoomController> postReplace)
+	// see https://gamedev.stackexchange.com/questions/86863/calculating-the-bounding-box-of-a-game-object-based-on-its-children
+	private Bounds CalculateBounds()
 	{
-		if (!remove)
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		if (renderers.Length == 0)
+		{
+			return new Bounds(transform.position, Vector3.zero);
+		}
+		Bounds b = renderers[0].bounds;
+		foreach (Renderer r in renderers)
+		{
+			b.Encapsulate(r.bounds);
+		}
+		return b;
+	}
+
+	private void MaybeReplaceDoor(ref bool isOpen, Bounds bounds, Vector3 replaceOffset, Vector3 checkSize, GameObject door, Action<RoomController> postReplace)
+	{
+		bool spawnedFromThisDirection = isOpen;
+		bool canSpawnRoom = !spawnedFromThisDirection && m_spawnDepthMax > 0 && Physics2D.OverlapBox(bounds.center + replaceOffset, checkSize, 0.0f) == null;
+		Assert.IsTrue(!spawnedFromThisDirection || !canSpawnRoom);
+		isOpen = spawnedFromThisDirection || (canSpawnRoom && UnityEngine.Random.value > m_roomSpawnPct);
+
+		if (!isOpen)
 		{
 			door.GetComponent<BoxCollider2D>().enabled = true; // TODO: why are instantiated door colliders sometimes disabled?
 			return;
@@ -69,12 +97,14 @@ public class RoomController : MonoBehaviour
 
 		Destroy(door);
 
-		if (!replace)
+		if (!canSpawnRoom)
 		{
 			return;
 		}
 
-		GameObject newRoom = Instantiate(m_roomPrefab, transform.position + replaceOffset, Quaternion.identity);
-		postReplace(newRoom.GetComponent<RoomController>());
+		GameObject newRoomObj = Instantiate(m_roomPrefab, transform.position + replaceOffset, Quaternion.identity);
+		RoomController newRoom = newRoomObj.GetComponent<RoomController>();
+		newRoom.m_spawnDepthMax = m_spawnDepthMax - 1;
+		postReplace(newRoom);
 	}
 }
