3a991a2e82bc232885977d18616969dfdefee35a
bendious <benjamin.laws@comcast.net>
2022-01-11 09:43:53 -0600

    Inventory click handling
    - added click handler to InventoryController
    	- implemented IPointerClickHandler/OnPointerClick()
    - logic to swap clicked inventory object to the primary slot
    	- private SwapWithIndex() helper from logic formerly in OnEndDrag()

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Platformer.Mechanics;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class InventoryController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class InventoryController : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 	public float m_smoothTime = 0.05f;
 	public float m_smoothEpsilon = 0.01f;
@@ -20,6 +21,20 @@ public class InventoryController : MonoBehaviour, IBeginDragHandler, IDragHandle
 	private void Start()
 	{
 		m_restPosition = transform.position;
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		// ignore if we're empty or the player is dragging
+		if (!m_draggable || eventData.dragging)
+		{
+			return;
+		}
+
+		if (transform.GetSiblingIndex() > 1) // NOTE the 1-based indexing due to template object
+		{
+			SwapWithIndex(1);
+		}
 	}
 
 	public void OnBeginDrag(PointerEventData eventData)
@@ -52,31 +67,46 @@ public class InventoryController : MonoBehaviour, IBeginDragHandler, IDragHandle
 				continue;
 			}
 
-			// get indices
-			int startIdx = transform.GetSiblingIndex();
+			// get other index and swap
 			int endIdx = endElement.transform.GetSiblingIndex();
-
-			// swap order
-			transform.SetSiblingIndex(endIdx);
-			endElement.transform.SetSiblingIndex(startIdx);
-			AvatarController avatar = Camera.main.GetComponent<GameController>().m_avatar;
-			Transform startTf = avatar.transform.GetChild(startIdx - 1);
-			Transform endTf = avatar.transform.GetChild(endIdx - 1);
-			startTf.SetSiblingIndex(endIdx - 1);
-			endTf.SetSiblingIndex(startIdx - 1);
-
-			// swap positions
-			Vector3 tmp = m_restPosition;
-			m_restPosition = endElement.m_restPosition;
-			endElement.m_restPosition = tmp;
-			StartCoroutine(endElement.LerpToRest());
-			break;
+			SwapWithIndex(endIdx);
+			return;
 		}
 
-		// slide to old/new position
+		// if no swap, slide to old position
 		StartCoroutine(LerpToRest());
 	}
 
+
+	private void SwapWithIndex(int index2)
+	{
+		int index1 = transform.GetSiblingIndex();
+		Assert.AreNotEqual(index1, index2);
+		InventoryController element2 = transform.parent.GetChild(index2).GetComponent<InventoryController>();
+
+		// swap inventory order
+		transform.SetSiblingIndex(index2);
+		element2.transform.SetSiblingIndex(index1);
+
+		// swap avatar hold order
+		// NOTE that the indices are off by one between the inventory and avatar due to the inventory template object
+		AvatarController avatar = Camera.main.GetComponent<GameController>().m_avatar;
+		int idx1Final = index1 - 1;
+		int idx2Final = index2 - 1;
+		Transform tf1 = avatar.transform.GetChild(idx1Final);
+		Transform tf2 = avatar.transform.GetChild(idx2Final);
+		tf1.SetSiblingIndex(idx2Final);
+		tf2.SetSiblingIndex(idx1Final);
+
+		// swap icon positions
+		Vector3 tmp = m_restPosition;
+		m_restPosition = element2.m_restPosition;
+		element2.m_restPosition = tmp;
+
+		// slide to new positions
+		StartCoroutine(LerpToRest());
+		StartCoroutine(element2.LerpToRest());
+	}
 
 	private IEnumerator LerpToRest()
 	{
