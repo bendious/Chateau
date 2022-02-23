26905a27ae41a7f87acfc5db69fef9641d6cd083
bendious <benjamin.laws@comcast.net>
2022-02-23 16:26:44 -0600

    Material variety
    - added {Flesh/MetalThick/Paper/Plastic/Stone/Wood}.physicsMaterial2D
    - added/updated entries in MaterialSystem.asset
    - assigned/overrode appropriate materials on prefab colliders / rigid bodies
    
    - updated MaterialSystem.Find() to return a backup/null entry if no entry for the given material exists
    - updated MaterialSystem.PairBestMatch() to choose randomly from the appropriate options when returning a partial match
    
    Todo: more material-specific audio clips

--- Assets/Scripts/MaterialSystem.cs
+++ Assets/Scripts/MaterialSystem.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 
 [CreateAssetMenu(menuName = "ScriptableObject/Material System", fileName = "MaterialSystem", order = 0)]
@@ -13,12 +12,23 @@ public class MaterialSystem : ScriptableObject
 
 	public MaterialInfo Find(PhysicsMaterial2D material)
 	{
-		return m_materialInfos.First(info => info.m_material == material); // TODO: generic backup info? duplicate prevention?
+		// TODO: duplicate prevention/detection?
+		MaterialInfo backup = null;
+		MaterialInfo match = m_materialInfos.FirstOrDefault(info =>
+		{
+			if (info.m_material == null)
+			{
+				Debug.Assert(backup == null);
+				backup = info;
+			}
+			return info.m_material == material;
+		});
+		return match ?? backup;
 	}
 
 	public MaterialPairInfo PairBestMatch(PhysicsMaterial2D material1, PhysicsMaterial2D material2)
 	{
-		MaterialPairInfo partialMatch = null;
+		System.Collections.Generic.List<MaterialPairInfo> partialMatches = new();
 		MaterialPairInfo backupInfo = null; // generic backup info to ensure we always have something to use
 
 		foreach (MaterialPairInfo info in m_materialPairInfos)
@@ -34,19 +44,17 @@ public class MaterialSystem : ScriptableObject
 				return info; // full match // TODO: duplicate prevention/handling?
 			}
 
-			// skip partial match logic if possible
-			// TODO: prioritize between multiple partial matches?
-			if (partialMatch != null || (info.m_material1 != null && info.m_material2 != null))
+			if (info.m_material1 != null && info.m_material2 != null)
 			{
-				continue;
+				continue; // fully-specified entries are too specific for partial matching
 			}
 
 			if (info.m_material1 == material1 || info.m_material2 == material2 || info.m_material1 == material2 || info.m_material2 == material1)
 			{
-				partialMatch = info;
+				partialMatches.Add(info);
 			}
 		}
 
-		return partialMatch ?? backupInfo;
+		return partialMatches.Count > 0 ? partialMatches[Random.Range(0, partialMatches.Count)] : backupInfo; // TODO: partial match priority?
 	}
 }
