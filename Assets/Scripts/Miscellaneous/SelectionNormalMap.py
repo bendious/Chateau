b99a7dc3628b24b7991f3cb5fdd2a72e05a37ad8
bendious <benjamin.laws@comcast.net>
2022-11-07 12:50:22 -0600

    Normal mapping helper script fixes
    - updated SelectionNormalMap.py to:
    	- use the full image size rather than assuming that the layer's size matches the image's
    		- removed layer param from helper functions
    	- not ignore less-than-half-selected pixels
    	- fix an missing/implicit tab alignment
    	- handle layer offsets
    	- prevent the final step from EdgeRange() being nullified by FalloffPct()
    	- default fromCenter to False for ease of use

--- Assets/Scripts/Miscellaneous/SelectionNormalMap.py
+++ Assets/Scripts/Miscellaneous/SelectionNormalMap.py
@@ -5,31 +5,31 @@ from gimpfu import *
 import math
 
 
-def IsNotSelected(image, layer, x, y):
-	# TODO: option to wrap edges?
-	return x < 0 or y < 0 or x >= layer.width or y >= layer.height or image.selection.get_pixel(x, y)[0] < 128 # TODO: don't assume equivalent image/layer sizes
+def IsNotSelected(image, x, y):
+	# TODO: option to wrap edges? take selection amount into account?
+	return x < 0 or y < 0 or x >= image.width or y >= image.height or image.selection.get_pixel(x, y)[0] == 0
 
 def EdgeRange(edgeWidth, stepSize):
-	return range(1, int(edgeWidth / stepSize) + 1) # TODO: round?
+	return range(1, int(round(edgeWidth / stepSize)) + 1)
 
 def FalloffPct(stepPct, linearFalloff):
 	return 1.0 - stepPct if linearFalloff else math.cos(stepPct * math.pi * 0.5)
 
-def ColorizeFromCenter(image, layer, x, y, pixel, edgeWidth, stepSize, centerX, centerY, linearFalloff, invert):
+def ColorizeFromCenter(image, x, y, pixel, edgeWidth, stepSize, centerX, centerY, linearFalloff, invert):
 	diffX = x - centerX
 	diffY = y - centerY
 	diffMag = math.sqrt(diffX**2 + diffY**2)
 	if (diffMag > 0.0):
 		diffX /= diffMag
 		diffY /= diffMag
-
+	
 	xItr = x
 	yItr = y
 	for i in EdgeRange(edgeWidth, stepSize):
 		xItr += diffX * stepSize
 		yItr += diffY * stepSize
-		if (IsNotSelected(image, layer, int(round(xItr, 0)), int(round(yItr, 0)))):
-			pct = FalloffPct(i * stepSize / edgeWidth, linearFalloff)
+		if (IsNotSelected(image, int(round(xItr, 0)), int(round(yItr, 0)))):
+			pct = FalloffPct((i - 1) * stepSize / edgeWidth, linearFalloff)
 			if (invert):
 				pct = -pct;
 			pixel[0] += diffX * pct
@@ -37,21 +37,23 @@ def ColorizeFromCenter(image, layer, x, y, pixel, edgeWidth, stepSize, centerX, 
 			break;
 	return pixel
 
-def CheckEdge(image, layer, x, y, pixel, xDiff, yDiff, invert):
+def CheckEdge(image, x, y, pixel, xDiff, yDiff, invert):
 	# TODO: take selection amount into account
-	if (IsNotSelected(image, layer, x, y)):
+	if (IsNotSelected(image, x, y)):
 		pixel[0] += -xDiff if invert else xDiff
 		pixel[1] -= -yDiff if invert else yDiff # NOTE the -= due to Y=0 being the image top
 	return pixel
 
 def SelectionNormalMap(image, layerOrChannel, edgeWidth, stepSize, fromCenter, centerX, centerY, linearFalloff, invert):
 	# create new layer to copy to for safety
-	layer = gimp.Layer(image, "selectionNormalMap", layerOrChannel.width, layerOrChannel.height, RGBA_IMAGE) # TODO: don't assume equivalent image/layer sizes
+	layer = gimp.Layer(image, "selectionNormalMap", image.width, image.height, RGBA_IMAGE)
 	
-	for x in range(layer.width): # TODO: don't assume equivalent image/layer sizes
-		for y in range(layer.height):
-			if (IsNotSelected(image, layer, x, y)):
-				layer.set_pixel(x, y, (0, 0, 0, 0))
+	for xLocal in range(layer.width):
+		for yLocal in range(layer.height):
+			x = xLocal + layer.offsets[0]
+			y = yLocal + layer.offsets[1]
+			if (IsNotSelected(image, x, y)):
+				layer.set_pixel(xLocal, yLocal, (0, 0, 0, 0))
 				continue
 			
 			# base vector is pure Z
@@ -59,23 +61,24 @@ def SelectionNormalMap(image, layerOrChannel, edgeWidth, stepSize, fromCenter, c
 			
 			# nudge vector based on surrounding selection
 			if (fromCenter):
-				pixelFNeg = ColorizeFromCenter(image, layer, x, y, pixelFNeg, edgeWidth, stepSize, centerX, centerY, linearFalloff, invert)
+				pixelFNeg = ColorizeFromCenter(image, x, y, pixelFNeg, edgeWidth, stepSize, centerX, centerY, linearFalloff, invert)
 			else:
 				# TODO: efficiency
 				invSqrtTwo = 1.0 / math.sqrt(2.0)
 				for i in EdgeRange(edgeWidth, stepSize):
-					d = int(round(i * stepSize, 0)) # TODO: round after applying to x/y?
-					falloff = FalloffPct(d / edgeWidth, linearFalloff)
+					dFloat = i * stepSize
+					d = int(round(dFloat, 0)) # TODO: round after applying to x/y?
+					falloff = FalloffPct((dFloat - stepSize) / edgeWidth, linearFalloff)
 					falloffDiagonal = falloff * invSqrtTwo
 					
-					pixelFNeg = CheckEdge(image, layer, x - d, y, pixelFNeg, -falloff, 0.0, invert)
-					pixelFNeg = CheckEdge(image, layer, x + d, y, pixelFNeg, falloff, 0.0, invert)
-					pixelFNeg = CheckEdge(image, layer, x, y - d, pixelFNeg, 0.0, -falloff, invert)
-					pixelFNeg = CheckEdge(image, layer, x, y + d, pixelFNeg, 0.0, falloff, invert)
-					pixelFNeg = CheckEdge(image, layer, x - d, y - d, pixelFNeg, -falloffDiagonal, -falloffDiagonal, invert)
-					pixelFNeg = CheckEdge(image, layer, x + d, y + d, pixelFNeg, falloffDiagonal, falloffDiagonal, invert)
-					pixelFNeg = CheckEdge(image, layer, x - d, y + d, pixelFNeg, -falloffDiagonal, falloffDiagonal, invert)
-					pixelFNeg = CheckEdge(image, layer, x + d, y - d, pixelFNeg, falloffDiagonal, -falloffDiagonal, invert)
+					pixelFNeg = CheckEdge(image, x - d, y, pixelFNeg, -falloff, 0.0, invert)
+					pixelFNeg = CheckEdge(image, x + d, y, pixelFNeg, falloff, 0.0, invert)
+					pixelFNeg = CheckEdge(image, x, y - d, pixelFNeg, 0.0, -falloff, invert)
+					pixelFNeg = CheckEdge(image, x, y + d, pixelFNeg, 0.0, falloff, invert)
+					pixelFNeg = CheckEdge(image, x - d, y - d, pixelFNeg, -falloffDiagonal, -falloffDiagonal, invert)
+					pixelFNeg = CheckEdge(image, x + d, y + d, pixelFNeg, falloffDiagonal, falloffDiagonal, invert)
+					pixelFNeg = CheckEdge(image, x - d, y + d, pixelFNeg, -falloffDiagonal, falloffDiagonal, invert)
+					pixelFNeg = CheckEdge(image, x + d, y - d, pixelFNeg, falloffDiagonal, -falloffDiagonal, invert)
 			
 			# normalize and return to [0,255]
 			pixelMag = math.sqrt(pixelFNeg[0]**2 + pixelFNeg[1]**2 + pixelFNeg[2]**2)
@@ -83,7 +86,7 @@ def SelectionNormalMap(image, layerOrChannel, edgeWidth, stepSize, fromCenter, c
 			pixelNormF = (pixelNormFNeg[0] * 0.5 + 0.5, pixelNormFNeg[1] * 0.5 + 0.5, pixelNormFNeg[2] * 0.5 + 0.5, pixelNormFNeg[3])
 			pixelNormI = (int(pixelNormF[0] * 255.0), int(pixelNormF[1] * 255.0), int(pixelNormF[2] * 255.0), int(pixelNormF[3] * 255.0))
 			
-			layer.set_pixel(x, y, pixelNormI)
+			layer.set_pixel(xLocal, yLocal, pixelNormI)
 	
 	# TODO: blend pixels inward?
 	
@@ -103,7 +106,7 @@ register("SelectionNormalMap",
 		[
 			(PF_FLOAT, "edgeWidth", "Edge Width", 1.0),
 			(PF_FLOAT, "stepSize", "Step Size", 1.0),
-			(PF_BOOL, "fromCenter", "From Center", True),
+			(PF_BOOL, "fromCenter", "From Center", False),
 			(PF_FLOAT, "centerX", "Center X", 0.0),
 			(PF_FLOAT, "centerY", "Center Y", 0.0),
 			(PF_BOOL, "linearFalloff", "Linear Falloff", False),
