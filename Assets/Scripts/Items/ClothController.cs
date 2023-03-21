327e8a32f29445eef7bfa552bbfe1534cd8f430f
bendious <benjamin.laws@comcast.net>
2023-03-21 13:36:45 -0500

    Cloth test shirt & nightcap
    - {Nightcap/ShirtCheckered}.prefab
    	- Prefabs/Cloth/
    	- {Nightcap/ShirtCheckered}.obj
    		- updated .gitignore to allow tracking .obj files
    	- Checkered.mat
    		- checkers.png
    	- ClothController component to update cloth object position based on KinematicCharacter.{Head/Arm}Offset
    - added 3D colliders to Arm/Avatar/Shadow for collision w/ Cloth
    - adjusted Z spacing of character arms and backpacks for correct render order w/ cloth
    
    Todo: fix cloth self-occlusion / render depth & alpha mirroring, non-debug placement/pickup/etc, clothing variety

---
+++ Assets/Scripts/Items/ClothController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+
+public class ClothController : MonoBehaviour
+{
+	[SerializeField] private Vector3 m_offset;
+	[SerializeField] private bool m_offsetFromHead;
+
+
+	private KinematicCharacter m_character;
+
+	private bool m_wasLeftFacing;
+
+
+	private void Start()
+	{
+		m_character = GetComponentInParent<KinematicCharacter>(); // TODO: handle attachment/detachment
+	}
+
+	private void LateUpdate()
+	{
+		gameObject.transform.localPosition = (Vector3)(m_offsetFromHead ? m_character.HeadOffset : m_character.ArmOffset) + (m_character.LeftFacing ? new(-m_offset.x, m_offset.y, m_offset.z) : m_offset);
+		if (m_wasLeftFacing != m_character.LeftFacing)
+		{
+			gameObject.transform.localRotation = m_character.LeftFacing ? Quaternion.Euler(0.0f, 180.0f, 0.0f) : Quaternion.identity;
+			m_wasLeftFacing = m_character.LeftFacing;
+		}
+	}
+}
