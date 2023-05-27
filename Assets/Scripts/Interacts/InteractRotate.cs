eb17582533d4ff450b7fb9f7322d33d4dcc27c48
bendious <benjamin.laws@comcast.net>
2023-05-26 23:06:25 -0500

    Clock auto-ticking
    - InteractRotate logic to support auto-interacting at given intervals
    	- private serialized m_(autoInteract(Sec|Child)|avatarInteractTimeout)
    	- private m_lastAvatarInteractTime to prevent unlocking w/o being recently touched by any player
    	- private AutoInteract() coroutine started by Start()
    - updated LockClock.Control{1-4} with auto-tick setups

--- Assets/Scripts/Interacts/InteractRotate.cs
+++ Assets/Scripts/Interacts/InteractRotate.cs
@@ -7,6 +7,10 @@ public class InteractRotate : MonoBehaviour, IInteractable, IKey
 	[SerializeField] private GameObject m_rotator;
 	[SerializeField] private float m_degreesIncrement = 30.0f;
 
+	[SerializeField] private float m_autoInteractSec = -1.0f;
+	[SerializeField] private InteractRotate m_autoInteractChild;
+	[SerializeField] private float m_avatarInteractTimeout = 12.0f; // TODO: derive from m_degreesIncrement? player-facing messaging?
+
 	[SerializeField] private WeightedObject<AudioClip>[] m_sfx;
 	[SerializeField] private WeightedObject<AudioCollection>[] m_sfxGroups;
 
@@ -27,6 +31,8 @@ public class InteractRotate : MonoBehaviour, IInteractable, IKey
 
 	private AudioCollection m_sfxGroup;
 
+	private float m_lastAvatarInteractTime = float.MinValue;
+
 
 	private void Start()
 	{
@@ -42,6 +48,16 @@ public class InteractRotate : MonoBehaviour, IInteractable, IKey
 		}
 		Debug.Assert(360.0f.Modulo(m_degreesIncrement).FloatEqual(0.0f), "Uneven rotation increment?");
 		m_rotator.transform.rotation = Quaternion.Euler(0.0f, 0.0f, m_degreesIncrement * Random.Range(0, Mathf.RoundToInt(360 / m_degreesIncrement)));
+
+		if (m_autoInteractSec > 0.0f)
+		{
+			StartCoroutine(AutoInteract(m_autoInteractSec));
+			if (m_autoInteractChild != null)
+			{
+				// if our auto-ticking is enabled, our children's should be disabled since we control them directly
+				m_autoInteractChild.m_autoInteractSec = float.MinValue;
+			}
+		}
 	}
 
 
@@ -51,8 +67,24 @@ public class InteractRotate : MonoBehaviour, IInteractable, IKey
 
 	public void Interact(KinematicCharacter interactor, bool reverse)
 	{
+		if (reverse && m_autoInteractChild != null && m_rotator.transform.rotation.eulerAngles.z.FloatEqual(0.0f))
+		{
+			m_autoInteractChild.Interact(interactor, reverse);
+		}
 		m_rotator.transform.rotation *= Quaternion.Euler(0.0f, 0.0f, reverse ? m_degreesIncrement : -m_degreesIncrement); // NOTE the reversal of the expected "reverse" semantics due to clockwise clock rotation // TODO: parameterize?
-		(Lock as LockController).CheckInput();
+		if (!reverse && m_autoInteractChild != null && m_rotator.transform.rotation.eulerAngles.z.FloatEqual(0.0f))
+		{
+			m_autoInteractChild.Interact(interactor, reverse);
+		}
+
+		if (interactor is AvatarController)
+		{
+			m_lastAvatarInteractTime = Time.time;
+		}
+		if (m_lastAvatarInteractTime + m_avatarInteractTimeout >= Time.time) // TODO: also check interact times on parent interactor(s)?
+		{
+			(Lock as LockController).CheckInput();
+		}
 
 		if (m_sfxGroup != null)
 		{
@@ -83,5 +115,18 @@ public class InteractRotate : MonoBehaviour, IInteractable, IKey
 	{
 		GetComponent<UnityEngine.Rendering.Universal.Light2D>().enabled = false;
 		enabled = false;
+	}
+
+
+	private System.Collections.IEnumerator AutoInteract(float seconds)
+	{
+		yield return new WaitForSeconds(Random.Range(0.0f, seconds));
+
+		WaitForSeconds wait = new(seconds);
+		while (enabled && m_autoInteractSec > 0.0f)
+		{
+			Interact(null, false);
+			yield return wait;
+		}
 	}
 }
